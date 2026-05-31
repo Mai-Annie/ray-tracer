17d7b2051cd6a3c933a4f62fa733e8b2b9606935
Annie <155664992+Mai-Annie@users.noreply.github.com>
2026-05-30 17:37:52 -0700

    added color utility functions and modified main.cpp

---
+++ color.h
@@ -0,0 +1,25 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+using color = vec3; // RGB color
+
+// Write the translated [0,255] value of each color component in the output stream
+void write_color(std::ostream& out, const color& pixel_color){
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Translate the color values from [0,1] to [0,255] (bytes)
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    // Write out the pixel color components
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+
+#endif
